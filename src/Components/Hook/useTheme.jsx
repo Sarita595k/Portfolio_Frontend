80dbe86a7ad898ed15dd2482e6ae15addc108abc
sarita595k <sarita595k@gmail.com>
2026-04-10 20:46:20 +0530

    chnage the useTheme preferences

--- src/Components/Hook/useTheme.jsx
+++ src/Components/Hook/useTheme.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react";
 
 export const useTheme = () => {
-    const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+
+    const [theme, setTheme] = useState(() => {
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme ? savedTheme : "dark";
+    });
 
     useEffect(() => {
         const root = window.document.documentElement;
+
         if (theme === "dark") {
             root.classList.add("dark");
         } else {
             root.classList.remove("dark");
         }
+
         localStorage.setItem("theme", theme);
     }, [theme]);
 
     const toggleTheme = () => {
-        setTheme(theme === "light" ? "dark" : "light");
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
     };
 
     return { theme, toggleTheme };
--- src/Page/LandingPage.jsx
+++ src/Page/LandingPage.jsx
@@ -1,25 +1,19 @@
 import About from '../Components/About/About'
-import AboutJourney from '../Components/About/AboutJourney'
 import Projects from '../Components/Projects/Projects'
 import "./LandingPage.css"
-import { useState } from 'react'
-import { FaMoon, FaSun } from 'react-icons/fa'
 import Skills from '../Components/Skills/Skills'
 import Footer from '../Components/Footer/footer'
 import Contact from '../Components/Contact/Contact'
 import Resume from '../Components/Experience/Resume'
 import Hero from '../Components/Header/Hero'
-import Navbar from '../Components/Navbar/Navbar'
 const LandingPage = () => {
 
     return (
         <div>
-            {/* <Navbar /> */}
             <Hero />
             <About />
             <Skills />
             <Resume />
-            {/* <AboutJourney /> */}
             <Projects />
             <Contact />
             <Footer />
